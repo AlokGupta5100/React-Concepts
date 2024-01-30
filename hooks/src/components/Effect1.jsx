c98bd9f648679e04756a04a4e9f599924415e9ac
Alok <alokgupta5100@gmail.com>
2024-01-30 23:50:54 +0530

    chore:fetch

--- hooks/src/components/Effect1.jsx
+++ hooks/src/components/Effect1.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 const Effect1 = () => {
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const   response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const result = await response.json();
+                setData(result);
+            }
+            catch(error) {
+                console.error('Error fetching data:', error)
+            }
+        }
+        fetchData();
+    },[]);
+
   return (
     <div>
-      <h2>srhrhd</h2>
+        <h1>Data Fetching</h1>
+        <ul>
+            {data.map((post) => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
     </div>
   )
 }
