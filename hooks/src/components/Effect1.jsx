83cdd0d8ec8db3f011a87f2e589c4cc111e95680
Alok Gupta <115941261+AlokGupta5100@users.noreply.github.com>
2024-01-31 00:05:46 +0530

    Revert "Chore/updatedbranch"

--- hooks/src/components/Effect1.jsx
+++ hooks/src/components/Effect1.jsx
@@ -1,33 +1,10 @@
 import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react'
 
 const Effect1 = () => {
 
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const   response = await fetch('https://jsonplaceholder.typicode.com/posts');
-                const result = await response.json();
-                setData(result);
-            }
-            catch(error) {
-                console.error('Error fetching data:', error)
-            }
-        }
-        fetchData();
-    },[]);
-
   return (
     <div>
-        <h1>Data Fetching from temp api</h1>
-        <ul>
-            {data.map((post) => (
-                <li key={post.id}>{post.title}</li>
-            ))}
-        </ul>
+      <h2>srhrhd</h2>
     </div>
   )
 }
